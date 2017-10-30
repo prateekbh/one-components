fee1a80c6faab8f90bb503ea6abf6f7aeebaa544
Prateek Bhatnagar <prateek89born@gmail.com>
2017-10-30 18:27:07 +0530

    Add button and checkbox

---
+++ material-element.js
@@ -0,0 +1,68 @@
+import { MDCRipple } from "@material/ripple";
+
+export default class MaterialElement extends HTMLElement{
+
+  constructor() {
+    super();
+
+    // This will again be used to add apt classname to the component
+    this.componentName_ = "";
+
+    // The final class name given to the dom
+    this.classText_ = "";
+
+    this.nativeAttributes_ = [];
+
+    // Attach Shadow dom
+    this.attachShadow({mode: 'open'});
+  }
+
+  // Build the className
+  buildClassName_(props) {
+    this.classText_ = "mdc-" + this.componentName_;
+    this.nativeAttributes_ = [];
+    const attrs = this.getAttributeNames();
+    const observedAttributes = this.observedAttributes || [];
+    for (let attrKey in attrs) {
+      if (attrs.hasOwnProperty(attrKey)) {
+        const attr = attrs[attrKey];
+        if (observedAttributes.includes(attr)) {
+          this.classText_ += " mdc-" + this.componentName_ + "--" + attr;
+        } else {
+          this.nativeAttributes_.push(`${attr}='${this.getAttribute(attr)}'`);
+        }
+      }
+    }
+  }
+
+  bypassDOMEvents_(node, eventsList) {
+    eventsList.forEach((event) => {
+      this.addEventListener(event, (e) => {
+        e.preventDefault();
+      });
+
+      node.addEventListener(event, (e) => {
+        this.dispatchEvent(new Event(event), e);
+      });
+    });
+  }
+
+  buildDom_(classes) {
+    return `<div class='${classes}'><slot></slot></div>`;
+  }
+
+  renderDom_() {
+    this.buildClassName_();
+    const classes = this.classText_;
+    this.shadowRoot.innerHTML = this.buildDom_({classes});
+  }
+
+  connectedCallback() {
+    this.renderDom_();
+  }
+
+  attributeChangedCallback(attrName, oldVal, newVal){
+    console.log('changed', attrName);
+    this.renderDom_();
+  }
+}
